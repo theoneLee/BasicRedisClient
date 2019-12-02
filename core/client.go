e5979e771f03d0001e36efcbf624d59998a1b136
theoneLee <623507020@qq.com>
2019-12-02 18:35:57 +0800

    parse

--- core/client.go
+++ core/client.go
@@ -1,6 +1,9 @@
 package core
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"net"
 	"strconv"
 )
@@ -38,5 +41,78 @@ func multiCommandMarshal(args ...string) string {
 }
 
 func (r *Reply) Reply() {
+	reader := bufio.NewReader(r.Conn)
+	b, err := reader.Peek(1) // pre read a first byte to judge what redis response
+	if err != nil {
+		fmt.Println("conn err")
 
+	}
+
+	if b[0] == byte('*') {
+		r.IsMulti = true //it means redis response array ,see https://redis.io/topics/protocol#resp-arrays
+		r.MultiReply, r.Err = multiResponse(reader)
+	} else { //b[0] will be '+' '-' ':' '$'
+		r.IsMulti = false
+		r.SingleReply, err = singleResponse(reader)
+		if err != nil {
+			r.Err = err
+			return
+		}
+	}
+
+}
+
+func singleResponse(reader *bufio.Reader) (res []byte, err error) {
+	prefix, err := reader.ReadByte()
+	if err != nil {
+		return
+	}
+	switch prefix {
+	case byte('+'), byte('-'), byte(':'):
+		res, _, err = reader.ReadLine()
+	case byte('$'):
+		// $7\r\naiangwt\r\n
+		n, _, err := reader.ReadLine()
+		if err != nil {
+			return
+		}
+		l, err := strconv.Atoi(string(n))
+		if err != nil {
+			return
+		}
+		p := make([]byte, l+2)
+		reader.Read(p)
+		res = p[0 : len(p)-2]
+
+	}
+	return
+}
+
+func multiResponse(reader *bufio.Reader) (res [][]byte, err error) {
+	prefix, err := reader.ReadByte()
+	if err != nil {
+		return
+	}
+	if prefix != byte('*') {
+		return res, errors.New("not multi response")
+	}
+	//*3\r\n$1\r\n3\r\n$1\r\n2\r\n$1\r\n
+	l, _, err := reader.ReadLine()
+	if err != nil {
+		return
+	}
+	n, err := strconv.Atoi(string(l))
+	if err != nil {
+		return
+	}
+	for i := 0; i < n; i++ {
+		s, err := singleResponse(reader)
+		fmt.Println("i =", i, "result = ", string(s))
+		if err != nil {
+			return
+		}
+		res = append(res, s)
+	}
+
+	return
 }
